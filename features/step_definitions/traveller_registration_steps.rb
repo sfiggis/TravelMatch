456a4d2d35f93fc58dea0408aea19959961e7815
Sophie Figgis <sophiefiggis@gmail.com>
2017-08-14 11:02:43 +0100

    added test for traveller registration

---
+++ features/step_definitions/traveller_registration_steps.rb
@@ -0,0 +1,40 @@
+Given(/^they are on the sign up page$/) do
+  visit new_traveller_registration_path
+end
+
+When(/^they enter their username$/) do
+  fill_in 'Username', :with => 'test_username'
+end
+
+When(/^they enter their email$/) do
+ fill_in 'Email', :with => 'test@example.com'
+end
+
+When(/^they enter their password$/) do
+  fill_in 'Password', :with => 'password'
+end
+
+When(/^they confirm their password$/) do
+  fill_in 'Password confirmation', :with => 'password'
+end
+
+When(/^they register$/) do
+  click_on 'Sign up'
+end
+
+Then(/^they have a new account$/) do
+  expect(Traveller.count).to eq 1
+end
+
+Then(/^they get sent to the homepage$/) do
+  expect(page.current_path).to eq '/'
+end
+
+Then(/^they see a message welcoming them into Travel Match$/) do
+  expect(page).to have_content("Welcome to Travel Match!")
+end
+
+Then(/^they receive an email asking them to confirm their email address$/) do
+  expect(ActionMailer::Base.deliveries.length).to eq 1
+  expect(ActionMailer::Base.deliveries.first.subject).to match 'Welcome to Travel Match!'
+end
---
+++ features/traveller_registration.feature
@@ -0,0 +1,13 @@
+Feature: Traveller Registration
+  @wip
+  Scenario: A traveller registers with the site
+    Given they are on the sign up page
+    When they enter their username
+      And they enter their email
+      And they enter their password
+      And they confirm their password
+      And they register
+    Then they have a new account
+      And they get sent to the homepage
+      And they see a message welcoming them into Travel Match
+      And they receive an email asking them to confirm their email address
