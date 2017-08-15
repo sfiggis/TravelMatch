f6988124fac1e9b7e04b2e6aa9674b3e19c7ddd9
Sophie Figgis <sophiefiggis@gmail.com>
2017-08-15 22:53:40 +0100

    added create action for searches -data being sent from react, and sent to model method to send to api, and then saved as route, and redirected to show controller

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,24 @@
+require 'pry'
+class SearchesController < ApplicationController
+  def show
+    binding.pry
+    @search = Search.find(params[:id])
+    render json: @search
+  end
+  def create
+    @search = Search.new(search_params)
+    response = @search.results
+    @search.save
+    binding.pry
+    redirect_to search_path(@search)
+  end
+
+  private
+  def search_params
+    params.require(:search).permit(:departureDate, :returnDate, :travellerId, :origin).transform_keys { |k| k.underscore }
+  end
+
+  def find_search
+    @search = Search.find(params[:id])
+  end
+end
