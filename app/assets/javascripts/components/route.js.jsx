9a65ca79d8a39415e0e73d92617d3b1b8573f410
Sophie Figgis <sophiefiggis@gmail.com>
2017-08-16 16:10:47 +0100

    new nested react components for search, routelist and routes

--- app/assets/javascripts/components/route.js.jsx
+++ app/assets/javascripts/components/route.js.jsx
@@ -1,4 +1,17 @@
-class Route extends React.component {
+var Route = React.createClass({
   render() {
+    return (
+      <div>
+      <h4>{ this.props.route.airline }</h4>
+      <h4>{ this.props.route.currency }</h4>
+      <h4>{ this.props.route.price }</h4>
+      <h4>{ this.props.route.departure_at }</h4>
+      <h4>{ this.props.route.return_at }</h4>
+      <h4>{ this.props.route.destination_code }</h4>
+      <h4>{ this.props.route.expires_at }</h4>
+      <h4>{ this.props.route.currency }</h4>
+
+      </div>
+    )
   }
-}
+});
--- app/assets/javascripts/components/routeList.js.jsx
+++ app/assets/javascripts/components/routeList.js.jsx
@@ -1,5 +1,24 @@
-class RouteList extends React.component {
+var RouteList = React.createClass({
   render() {
-    return (<Route />)
-  }
-}
+
+    if(this.props.parent.props.routes !== undefined) {
+      const listItems = this.props.parent.props.routes.map((route) =>
+      <li className="route" key={ route.id }>
+        <Route route={ route } />
+      </li>
+    );
+      return (
+        <div>
+          <h1>Routes</h1>
+          <ul className="project-listings">
+            { listItems }
+          </ul>
+        </div>
+      )
+    } else {
+        return (
+          <h2>Waiting...</h2>
+        )
+      }
+    }
+});
