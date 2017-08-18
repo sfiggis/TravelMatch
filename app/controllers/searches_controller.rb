3fe22b33bd79adcaaa11f7d1af053a3eda5e9c33
Sophie Figgis <sophiefiggis@gmail.com>
2017-08-18 13:37:57 +0100

    oved logic out of controller into mode

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,9 +5,10 @@ class SearchesController < ApplicationController
     # render json: @search
   end
   def create
-    binding.pry
+    Country.current_traveller = find_traveller
     @search = Search.new(search_params)
-    response = @search.results
+    destinations = @search.destination_results(search_params[:budget])
+    response = @search.flight_results
     @search.save
     # redirect_to search_path(@search)
     render "show"
@@ -15,10 +16,14 @@ class SearchesController < ApplicationController
 
   private
   def search_params
-    params.require(:search).permit(:departureDate, :returnDate, :travellerId, :origin, :budget).transform_keys { |k| k.underscore }
+    params.require(:search).permit(:departureDate, :returnDate, :travellerId, :origin, :budget, :traveller_id).transform_keys { |k| k.underscore }
   end
 
   def find_search
     @search = Search.find(params[:id])
+  end
+
+  def find_traveller
+    Traveller.find(params[:search][:travellerId])
   end
 end
