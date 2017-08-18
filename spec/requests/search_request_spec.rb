3cef695bd54392ee528ed6421b227124c300a50b
Sophie Figgis <sophiefiggis@gmail.com>
2017-08-18 13:39:01 +0100

    testing destination matching on basis of budget/gdp/time available/cost per day and bringing back airports for capital cities

--- spec/requests/search_request_spec.rb
+++ spec/requests/search_request_spec.rb
@@ -6,15 +6,27 @@ RSpec.describe "SearchRequest", :type => :request do
       @traveller = Traveller.new(first_name: "Joe", last_name: "Bloggs", username: "joey_blogger", email: "email@example.co.uk", password: "password", password_confirmation: "password")
       @traveller.skip_confirmation!
       @traveller.save!
+      @norway = Country.create!(name: "Norway", iso2: "NO", iso3: "NOR", itu: "NOR", fips: "NO", currency_code: "NOK", currency_name: "Norweigan Crone", capital: "Oslo", continent: "EU", gdp_ppp: 59301.6702)
+      @traveller.traveller_countries.create!(country_id: @norway.id, home: true)
+      Country.token = "12345"
+        stub_request(:get, "https://openexchangerates.org/api/latest.json?app_id=12345").
+        with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+        to_return(status: 200, body: File.read("spec/exchange_rates.json"), headers: {})
       Search.api_key = "12345"
-      stub_request(:get, "http://api.travelpayouts.com/v1/prices/cheap?departure_date=2017-11-01&format=json&limit=100&origin=MOW&return_date=2017-12-01&token=12345").
+         stub_request(:get, "http://api.travelpayouts.com/v1/prices/cheap?departure_date=2017-11-01&format=json&limit=100&origin=MOW&return_date=2017-11-03&token=12345").
          with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
          to_return(status: 200, body: File.read("spec/travel_payouts/cheapest_flights.json"), headers: {})
     end
     it "creates a search and routes" do
-      post "/searches", :params => { search: { departureDate: Date.new(2017,11, 01), returnDate: Date.new(2017,12, 01), origin: "MOW", travellerId: @traveller.id} }
+      post "/searches", :params => { search: { departureDate: Date.new(2017,11, 01), returnDate: Date.new(2017,11, 03), origin: "MOW", travellerId: @traveller.id, budget: 300} }
       expect(Search.count).to eq 1
       expect(Search.first.routes.count).to be > 0
+    end
+
+    it "finds destinations that fit search budget and are capitals" do
+      post "/searches", :params => { search: { departureDate: Date.new(2017,11, 01), returnDate: Date.new(2017,11, 03), origin: "MOW", travellerId: @traveller.id, budget: 300}}
+      expect(Search.count).to eq 1
+      expect(Search.first.airports.count).to be > 0
     end
   end
 end
