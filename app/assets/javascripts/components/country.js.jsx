dd2b7a2fbfd5a167c8901aeb9c007c05e5b7a985
Sophie Figgis <sophiefiggis@gmail.com>
2017-08-21 17:14:34 +0100

    passing search id as param onto country show page so can access it for api call

--- app/assets/javascripts/components/country.js.jsx
+++ app/assets/javascripts/components/country.js.jsx
@@ -1,8 +1,9 @@
 var Country = React.createClass({
   render() {
+    console.log(this.props)
     return (
       <div>
-      <a href={"/countries/" + this.props.country.id }><h4>{ this.props.country.name }</h4></a>
+      <a href={"/countries/" + this.props.country.id + "?search=" + this.props.search.id}><h4>{ this.props.country.name }</h4></a>
       </div>
     )
   }
