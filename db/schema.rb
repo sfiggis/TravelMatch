5496a4f097c0439a9598c053d78ba547f0070ace
Sophie Figgis <sophiefiggis@gmail.com>
2017-08-21 14:21:05 +0100

    adding currency_code to traveller so less database hits

--- db/schema.rb
+++ db/schema.rb
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20170821013005) do
     t.string "first_name"
     t.string "last_name"
     t.string "currency"
+    t.string "currency_code"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "username", default: "", null: false
