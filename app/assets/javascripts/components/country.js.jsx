d711e01733f93197975a6dee999928a13266f561
Sophie Figgis <sophiefiggis@gmail.com>
2017-08-21 14:29:45 +0100

    added link to country destination list - linking to country show page

--- app/assets/javascripts/components/country.js.jsx
+++ app/assets/javascripts/components/country.js.jsx
@@ -2,7 +2,7 @@ var Country = React.createClass({
   render() {
     return (
       <div>
-      <h4>{ this.props.country.name }</h4>
+      <a href={"/countries/" + this.props.country.id }><h4>{ this.props.country.name }</h4></a>
       </div>
     )
   }
