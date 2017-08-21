0619154320bfdc538b9035db28a8a517479185e7
Sophie Figgis <sophiefiggis@gmail.com>
2017-08-21 01:20:59 +0100

    removed current_traveller method added in home controller for test as now messing live behaviour

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,12 @@
 class HomeController < ApplicationController
   def index
+    binding.pry
     @traveller = current_traveller
     @search = @traveller.searches.create
   end
 
-def current_traveller
-  current_traveller = Traveller.first if Rails.env.test?
-end
+# def current_traveller
+#   current_traveller ||= Traveller.first if Rails.env.test?
+# end
 
 end
