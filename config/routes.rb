616541661a4324a797feb9e85d0c2aa4700bbb3b
Sophie Figgis <sophiefiggis@gmail.com>
2017-08-22 22:00:56 +0100

    adding admin model with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
   default_url_options :host => 'localhost'
   devise_for :travellers, :controllers => {:registrations => 'travellers/registrations'}
+  devise_for :admins, controllers: { sessions: 'admins/sessions' }
+  authenticate :traveller do
+    resources :travellers, only: [:new, :create, :edit, :update, :destroy]
+    resources :admins
+  end
+
+  devise_scope :admin do
+    get 'sign_in', to: 'admins/sessions#new'
+  end
+  authenticate :admin do
+    resources :admins
+    resources :countries, only: [:new, :create, :edit, :update, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :travellers
   resources :searches
-  root to: "home#index"
+  root to: "pages#index"
   resources :countries, :only => [:create, :show, :update]
   resources :searches do
     member do
