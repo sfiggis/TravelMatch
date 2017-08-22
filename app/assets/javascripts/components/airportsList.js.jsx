702d50d1736dc09cd6b16e05d4fa77cb7c8d6919
Sophie Figgis <sophiefiggis@gmail.com>
2017-08-22 19:47:17 +0100

    added new react componenents for retreiving flight data for matched countries

--- app/assets/javascripts/components/airportsList.js.jsx
+++ app/assets/javascripts/components/airportsList.js.jsx
@@ -1,7 +1,46 @@
 class AirportsList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      flights: [],
+      flightsList: ''
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.countryFlights = this.countryFlights.bind(this);
+  };
+
+  countryFlights() {
+    let data = JSON.stringify({
+      search_id: this.props.search.id,
+      country_id: this.props.country.id,
+      });
+      var view = this
+      $.ajax({
+        url: "/airports/" + this.props.search.id + "/capital_update",
+        type: 'PUT',
+        contentType: 'application/json',
+        data: data,
+        success: function(data, status, xhr) {
+          console.log(data["flight_results"]);
+          view.setState({
+            flights: data["flight_results"]
+          });
+          view.flightList();
+        }
+      })
+    };
+
+  flightList() {
+    this.setState({
+      flightsList: <FlightsList flights={ this.state.flights } country={ this.props.country } airports={ this.props.airports }/>
+
+    })
   }
+
+  handleClick() {
+    this.countryFlights();
+    this.flightList();
+  };
   render() {
     if(this.props.airports !== undefined && this.props.airports !== null) {
       const listItems = this.props.airports.map((airport) =>
@@ -9,7 +48,11 @@ class AirportsList extends React.Component {
         <Airport search={ this.props.search } airport={ airport } country={ this.props.country }/>
       </li>)
       return (
+        <div>
+        <button onClick={ this.handleClick } id="top-flights-button">Top Flights</button>
+        <div>{ this.state.flightsList }</div>
         <div>{listItems}</div>
+        </div>
       )
     } else {
       return(
