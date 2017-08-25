82b423534ba7a27557d50dd717d31ad542a04602
Sophie Figgis <sophiefiggis@gmail.com>
2017-08-25 09:01:43 +0100

    removing confirmable as email not setup in production

--- app/models/traveller.rb
+++ app/models/traveller.rb
@@ -2,7 +2,7 @@ class Traveller < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
   validates :username, uniqueness: true
 
   has_many :searches
